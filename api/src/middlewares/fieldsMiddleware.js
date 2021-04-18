8f3e36f6ef7c9257fdeda2e7469c98f2f470153e
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2021-04-18 13:32:06 -0300

    chore: addying an middleware to verify if some files are filled

---
+++ api/src/middlewares/fieldsMiddleware.js
@@ -0,0 +1,24 @@
+const verifyEmptyFields = require('../utils/verifyEmptyFields');
+
+function fieldsMiddleware(req, res, next) {
+  const fields = [
+    { fieldName: 'Nome', value: req.body.name },
+    { fieldName: 'Marca', value: req.body.brand },
+    { fieldName: 'Modelo', value: req.body.model },
+    { fieldName: 'Placa', value: req.body.licensePlate },
+    { fieldName: 'Data', value: req.body.date },
+    { fieldName: 'Horário', value: req.body.time },
+  ];
+
+  const emptyFields = verifyEmptyFields(fields);
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ Response: `os campos ${emptyFields} não podem ser vázios` });
+  }
+
+  return next();
+}
+
+module.exports = fieldsMiddleware;
