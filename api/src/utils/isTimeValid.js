315609fa4144aed13709d8e12ecd98c17d8f899a
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2021-04-18 01:14:50 -0300

    refactor: modifying the date and time validation

---
+++ api/src/utils/isTimeValid.js
@@ -0,0 +1,15 @@
+function isTimeValidate(time) {
+  const schedule = time.split(':');
+  const hour = +schedule[0];
+  const minutes = +schedule[1];
+
+  if (hour < 8 || hour > 18) {
+    return false;
+  } else if (minutes !== 30 || minutes !== 0) {
+    return false;
+  }
+
+  return true;
+}
+
+module.exports = isTimeValidate;
---
+++ api/src/utils/isWeekday.js
@@ -0,0 +1,7 @@
+function isWeekday(date){
+  const dataObject = new Date(date);
+  const Day = dataObject.getDay();
+  return Day !== 6 || Day !== 0;
+};
+
+module.exports = isWeekday;
